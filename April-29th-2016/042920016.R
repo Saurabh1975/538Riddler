7a542bbdf563725644019daca596dc8ecc521c2f
Saurabh1975 <rane.saurabh7@gmail.com>
2016-06-15 14:42:58 -0700

    Create 042920016.R

---
+++ April-29th-2016/042920016.R
@@ -0,0 +1,166 @@
+library(igraph)
+library(ggplot2)
+library(ggthemes)
+
+#Overwrite default FiveThirtyEight theme in ggthemes
+source('theme_fivethirtyeight.R')
+
+
+
+
+#Set Intitial population of proud partygoers to 0, amount of partygoers to 25
+proud.prop <-0
+n<-25
+#Set sequence of 
+e.seq<-seq(from = n-1, to = n*(n-1)/2, by = 1)
+
+output <- matrix(ncol=2, nrow=length(e.seq))
+
+
+iterations<-1
+
+
+# Generate random graph, fixed probability
+for(e in e.seq){
+  for(i in 1:50){
+    repeat {
+      
+      g <- erdos.renyi.game(n, e,
+                            type='gnm')
+      if (is.connected(g)) {
+        break
+      }
+    }
+    
+    
+    g.adj<-data.matrix(get.adjacency(g))
+    g.deg<-degree(g)
+    
+    g.friends <- sapply(1:nrow(g.adj), 
+                        function(x)
+                          sum(g.deg[x])>sum((g.adj[x,]*g.deg))/(rowSums(g.adj)[x])
+    )
+    if(sum(g.friends)/n>proud.prop){
+      proud.prop<-sum(g.friends)/n
+      print(paste0("Number of Edges: ",e))
+      print(paste0("Proportion of Proud: ",proud.prop))
+      output[iterations,]<-c(e,proud.prop) 
+    }
+  }
+  iterations<-iterations+1
+}
+
+
+
+output<-data.frame(output)
+p2 <- ggplot(output, aes(x = X1, y = X2)) +
+  geom_point(size=3,shape=21,color='coral',fill='coral')
+
+p2+theme_fivethirtyeight()+
+  labs(title='Total Friendships (Edges) versus Proportion Proud Partygoers (N=20)',
+       x='Total Friendships (Edges)',
+       y='Proportion Proud Partygoers')+
+  geom_vline(xintercept=300,linetype=2)+
+  geom_vline(xintercept=4,linetype=2)+
+  annotate("text", x =20, y = 1, label = "Min. Edges",color='firebrick') +
+  annotate("text", x =315, y = 1, label = "Max. Edges",color='firebrick') 
+
+
+
+
+
+output <- matrix(ncol=2, nrow=100-4)
+output<-data.frame(output)
+output$X1<-5:100
+output$X2<-output$X1*(output$X1-1)/2-1
+
+p2 <- ggplot(output, aes(x = X1, y = X2)) +
+  geom_point(size=3,shape=21,color='coral',fill='coral')
+
+
+
+p2+theme_fivethirtyeight()+
+  labs(title='Partygoers (N) versus Friendship at Maximum Proud Partygoers',
+       x='Partygoers (N)',
+       y='Friendship at Maximum Proud Partygoers')
+
+
+colnames(output)<-c('Partygoers (N)','Maximum Friendships (Edges')
+output[output$Partygoers %in% c(5,10,20,40,50,100),]
+print(xtable(output[output$Partygoers %in% c(5,10,20,40,50,100),]), type="html",
+      include.rownames=FALSE,
+      align='c')
+
+
+
+
+
+
+
+
+proud.prop <-0
+n.seq<-seq(from = 5, to = 100, by = 1)
+
+output <- matrix(ncol=2, nrow=length(n.seq))
+iterations<-1
+
+for(n in n.seq){
+  for(i in 1:10){
+    e<-n*(n-1)/2-1
+    g <- erdos.renyi.game(n, e,
+                          type='gnm')
+    
+    
+    
+    g.adj<-data.matrix(get.adjacency(g))
+    g.deg<-degree(g)
+    
+    g.friends <- sapply(1:nrow(g.adj), 
+                        function(x)
+                          sum(g.deg[x])>sum((g.adj[x,]*g.deg))/(rowSums(g.adj)[x])
+    )
+    
+    if(sum(g.friends)/n>proud.prop){
+      proud.prop<-sum(g.friends)/n
+      print(paste0("Number of Nodes: ",n))
+      print(paste0("Number of Edges: ",e))
+      print(paste0("Proportion of Proud: ",proud.prop))
+      
+    }
+  }
+  output[iterations,]<-c(n,proud.prop)
+  proud.prop <-0
+  iterations<-iterations+1
+}
+
+
+output <- data.frame(output)
+p2 <- ggplot(output, aes(x = X1, y = X2)) +
+  geom_point(size=3,shape=21,color='coral',fill='coral')
+
+
+
+p2+theme_fivethirtyeight()+
+  labs(title='Partygoers versus Proportion Proud Partygoers',
+       x='Partygoers (N)',
+       y='Proportion Proud Partygoers')
+
+library(xtable)
+
+colnames(output)<-c('Partygoers','Proportion Proud Partygoers')
+output[output$Partygoers %in% c(5,10,20,40,50,100),]
+print(xtable(output[output$Partygoers %in% c(5,10,20,40,50,100),]), type="html",
+      include.rownames=FALSE,
+      align='c')
+
+
+
+
+
+fit1 <- lm(y ~ x1 + x2 + x3 + x4, data=mydata)
+fit2 <- lm(y ~ x1 + x2)
+
+plot(g, layout=layout.fruchterman.reingold,
+     vertex.label=NA, vertex.size=5)
+
+
